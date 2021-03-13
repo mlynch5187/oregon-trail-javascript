e446c561955fd6ca962a43c9d9fb1601a426484e
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-13 10:12:45 -0700

    Needs 2 axles to travel

--- test/wagon-test.js
+++ test/wagon-test.js
@@ -193,7 +193,7 @@ describe('Wagon', function() {
     assert.isFalse(wagon.canTravel());
   });
 
-  it.skip('should not be able to travel with only 1 axle', function() {
+  it('should not be able to travel with only 1 axle', function() {
     var wagon = makeValidWagon();
 
     wagon.axles.pop();
